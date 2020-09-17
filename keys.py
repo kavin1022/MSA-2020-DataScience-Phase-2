22983176b8d16570e193a363327f2b7732b5b28f
kavin1022 <68765800+kavin1022@users.noreply.github.com>
2020-09-18 00:01:33 +1200

    Delete keys.py

--- keys.py
+++
@@ -1,5 +0,0 @@
-#These keys are examples only, they won't work.
-#This is what the keys file should contain.
-client_id='o0_Gis-uEoUJ6A'
-client_secret='qe39lzMxX1guHoSUIJ3ZFCB_ZIk'
-username='Space_H'
